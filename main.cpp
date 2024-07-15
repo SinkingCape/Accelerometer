cda81dfd248170dc82d20ca13f5d1131bb753890
SinkingCape <dvdguilfoyle@gmail.com>
2024-07-15 21:51:15 +0000

    Compute Angle

--- main.cpp
+++ main.cpp
@@ -1,23 +1,23 @@
 #include "mbed.h"
-#include "LSM6DSLSensor.h" 
+#include "LSM6DSLSensor.h"
+#include <cmath> // Include for mathematical functions
 
 #define PI 3.141592654
 
-static DevI2C devI2c(PB_11,PB_10);
-static LSM6DSLSensor acc_gyro(&devI2c,0xD4,D4,D5); // high address
+static DevI2C devI2c(PB_11, PB_10);
+static LSM6DSLSensor acc_gyro(&devI2c, 0xD4, D4, D5); // high address
 
-
-float computeAngle(int x, int y, int z){
-    float res = 0;
-
-    return res;
+float computeAngle(int x, int y, int z) {
+    // Compute the tilt angle with respect to the x-axis using the arctangent function
+    float angle = atan2(y, sqrt(x * x + z * z)) * 180.0 / PI;
+    return angle;
 }
 
 /* Simple main function */
 int main() {
     uint8_t id;
     int32_t axes[3];
-    float res=0;
+    float res = 0;
     acc_gyro.init(NULL);
 
     acc_gyro.enable_x();
@@ -28,13 +28,10 @@ int main() {
     printf("LSM6DSL accelerometer & gyroscope = 0x%X\r\n", id);
 
     while(1) {
-
         acc_gyro.get_x_axes(axes);
         res = computeAngle(axes[0], axes[1], axes[2]);
-        printf("LSM6DSL: %6d, %6d, %6d, %3.2f\r\n", axes[0], axes[1], axes[2], res);
-
+        printf("LSM6DSL: %6d, %6d, %6d, %3.2f degrees\r\n", axes[0], axes[1], axes[2], res);
 
         thread_sleep_for(2000);
-
     }
 }
